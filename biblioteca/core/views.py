6e220d710ef9f2d0cd216443c62b7dc5ca94ba40
Carlos Gil <carlosgilmds@gmail.com>
2024-10-08 21:58:08 -0300

    CBV - Livro/Autor

--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -1,7 +1,31 @@
+# views.py
 from rest_framework import generics
-from .models import Livro
-from .serializers import LivroSerializer
+from .models import Categoria, Autor, Livro
+from .serializers import CategoriaSerializer, AutorSerializer, LivroSerializer
 
+# Views para o modelo Categoria
+class CategoriaList(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-list"
+
+class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-detail"
+
+# Views para o modelo Autor
+class AutorList(generics.ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-list"
+
+class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-detail"
+
+# Views para o modelo Livro
 class LivroList(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
