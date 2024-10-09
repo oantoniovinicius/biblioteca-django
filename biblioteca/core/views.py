8a8018b3cdd3bf665bc3614219ccdb144fc065e5
oantoniovinicius <devantoniovinicius@gmail.com>
2024-10-08 22:02:01 -0300

    Livro Filter

--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -1,5 +1,6 @@
 # views.py
 from rest_framework import generics
+from django_filters import rest_framework as filters
 from .models import Categoria, Autor, Livro
 from .serializers import CategoriaSerializer, AutorSerializer, LivroSerializer
 
@@ -25,13 +26,24 @@ class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AutorSerializer
     name = "autor-detail"
 
+class LivroFilter(filters.FilterSet):
+    titulo = filters.CharFilter(lookup_expr='icontains') 
+    autor = filters.CharFilter(field_name='autor__nome', lookup_expr='^')  
+    categoria = filters.CharFilter(field_name='categoria__nome', lookup_expr='^')  
+
+    class Meta:
+        model = Livro
+        fields = ['titulo', 'autor', 'categoria']
+
 # Views para o modelo Livro
 class LivroList(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    name = "livro-list"
+    filterset_class = LivroFilter
 
 class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
     name = "livro-detail"
+    
+
