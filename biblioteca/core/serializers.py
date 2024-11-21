319ac0cb1f029314cd6a5510a31128efdd9290c4
Carlos Gil <carlosgilmds@gmail.com>
2024-11-21 20:55:04 -0300

    ColecaoSerializer

--- biblioteca/core/serializers.py
+++ biblioteca/core/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
-from .models import Categoria, Livro, Autor
+from .models import Categoria, Livro, Autor, Colecao
+from django.contrib.auth.models import User  # Importa o modelo User
 
 class CategoriaSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only = True)
-    nome = serializers.CharField(max_length = 100)
+    id = serializers.IntegerField(read_only=True)
+    nome = serializers.CharField(max_length=100)
     
-    def create (self, validated_data):
+    def create(self, validated_data):
         return Categoria.objects.create(**validated_data)
     
     def update(self, instance, validated_data):
         instance.nome = validated_data.get('nome', instance.nome)
         instance.save()
         return instance
-    
+
 class AutorSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     nome = serializers.CharField(max_length=100)
@@ -24,7 +25,7 @@ class AutorSerializer(serializers.Serializer):
         instance.nome = validated_data.get('nome', instance.nome)
         instance.save()
         return instance
-    
+
 class LivroSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     titulo = serializers.CharField(max_length=200)
@@ -43,3 +44,19 @@ class LivroSerializer(serializers.Serializer):
         instance.save()
         return instance
 
+class ColecaoSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    nome = serializers.CharField(max_length=100)
+    descricao = serializers.CharField(required=False)
+    colecionador = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  # Relacionamento com User
+    livros = serializers.PrimaryKeyRelatedField(queryset=Livro.objects.all(), many=True)  # Relacionamento com Livro
+    
+    def create(self, validated_data):
+        return Colecao.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.nome = validated_data.get('nome', instance.nome)
+        instance.descricao = validated_data.get('descricao', instance.descricao)
+        instance.colecionador = validated_data.get('colecionador', instance.colecionador)
+        instance.save()
+        return instance
